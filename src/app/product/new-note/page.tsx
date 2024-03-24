211b32c68c21cd8eb98e8bc439834174a9b41470
toni-akintola <oakintol@nd.edu>
2024-03-24 17:19:14 -0400

    minor fixes

--- src/app/product/new-note/page.tsx
+++ src/app/product/new-note/page.tsx
@@ -5,6 +5,7 @@ import { toast } from '@/components/ui/use-toast'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import React from 'react'
 import { Database } from '../../../../types_db'
+import Link from 'next/link'
 
 const UploadNote = () => {
   const supabase = createClientComponentClient<Database>()
@@ -12,6 +13,10 @@ const UploadNote = () => {
   return (
     <div className='max-w-6xl m-4 sm:m-10 flex flex-col gap-8 grow items-stretch'>
       <div className='h-40 flex flex-col justify-center items-center border-b pb-8'>
+        <h2>Files must be converted to markdown before being uploaded. An automatic converter will be added soon.</h2>
+        <Link
+        href="https://cloudconvert.com/md-converter"
+        >To convert files into markdown, use this link</Link>
         <FileUploadInput
           onChange={async e => {
             const selectedFile = e.target.files?.[0]
